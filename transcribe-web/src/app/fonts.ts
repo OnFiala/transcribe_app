55b63cd4f297f1926b9992a9ea554ee6d90a50db
OnFiala <fiala@fitnessinstitut.cz>
2025-10-20 20:55:16 +0200

    updated few issues

--- transcribe-web/src/app/fonts.ts
+++ transcribe-web/src/app/fonts.ts
@@ -1,8 +1,6 @@
-// src/app/fonts.ts
 import { Manrope } from "next/font/google";
 
-// Manrope je velmi podobný Aveniru a má dobrou čitelnost.
-// Přidáme latin-ext kvůli češtině a typické řezy pro UI.
+// Manrope ~ Avenir (velmi čitelný, SIL OFL)
 export const brandSans = Manrope({
   subsets: ["latin", "latin-ext"],
   weight: ["400", "500", "700"],
